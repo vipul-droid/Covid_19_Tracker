ef247c555ba2c2e020010fd9734b6077b3d875c8
Vipul Tiwari <vipulengg98@gmail.com>
2021-08-27 23:44:34 +0530

    Updates

---
+++ precache-manifest.f8ea22bac2f2bd6909d531e0bad73a40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea471b1aec1ff8c428322e2e45cd95c",
+    "url": "/Covid_19_Tracker/index.html"
+  },
+  {
+    "revision": "6c2c8a8a4503e3ca0c31",
+    "url": "/Covid_19_Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "a8a706102d2bbc31ba03",
+    "url": "/Covid_19_Tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "6c2c8a8a4503e3ca0c31",
+    "url": "/Covid_19_Tracker/static/js/2.57a12797.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid_19_Tracker/static/js/2.57a12797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a706102d2bbc31ba03",
+    "url": "/Covid_19_Tracker/static/js/main.7a51ad50.chunk.js"
+  },
+  {
+    "revision": "ad6a2bda3c72130e460d",
+    "url": "/Covid_19_Tracker/static/js/runtime-main.c199e5cd.js"
+  }
+]);
